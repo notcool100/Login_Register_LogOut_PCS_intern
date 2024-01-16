832909027a3172729ec52c197763e8a8bce0a372
notcool100 <anjaljoshi6@gmail.com>
2024-01-16 14:42:24 +0545

    login/register

---
+++ login/login/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿
+using login.Data;
+using login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace anjalweb.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly AppDbContext _db;
+
+		public object Session { get; private set; }
+
+		public HomeController(ILogger<HomeController> logger, AppDbContext db)
+		{
+			
+			_db = db;
+			
+		}
+
+		public IActionResult Index()
+		{
+			 if(HttpContext.Session.GetString("UserSession") != null)
+			{
+				ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+				return View();
+			}
+			else
+			{
+				return RedirectToAction("Login");
+			}
+				
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Login(User obj)
+		{
+			var MyUser = _db.Users.Where(x=>x.Email== obj.Email && x.Password==obj.Password).FirstOrDefault();
+			if (MyUser != null)
+			{
+				HttpContext.Session.SetString("UserSession",MyUser.Name);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				ViewBag.LoginFail = "Email/Password is Incorrect";
+				
+			}
+			return View();
+		}
+		public IActionResult Logout()
+		{
+			if (HttpContext.Session.GetString("UserSession") != null)
+			{
+				HttpContext.Session.Remove("UserSession");
+				return RedirectToAction("Login");
+			}
+			return View();
+		}
+		public IActionResult Register() {
+		return View();
+		}
+		[HttpPost]
+		public IActionResult Register(User obj)
+		{
+			if (ModelState.IsValid){
+
+                var MyUser = _db.Users.Where(x => x.Email == obj.Email).FirstOrDefault();
+                if (MyUser != null)
+                {
+                    ViewBag.RegisterFail = "This Email is Already Registered";
+                }
+                else
+                {
+                    _db.Users.Add(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+            }
+		
+			return View();
+
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ login/login/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace login.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		[DisplayName("Full Name")]
+		public string Name { get; set; }
+		[Required]
+		[DisplayName("Email")]
+		public string Email { get; set; }
+		[Required]
+		[DisplayName("Password")]
+		[MinLength(8, ErrorMessage = "The {0} must be at least {1} characters long.")]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "The password must include at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+		public string Password { get; set; }
+		[Required]
+		[DisplayName("Phone Number")]
+		public int PhoneNumber { get; set; }
+
+	}
+}
